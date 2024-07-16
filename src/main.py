d97429ef6b91a28c4db3a19fb2eb3792997e73cf
mvjkel1 <michal.semen99@gmail.com>
2024-07-16 19:16:29 +0200

    Refactored the structure of auth endpoints, added initial README

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
-from endpoints import auth, music
+from endpoints import client_auth, music, user_auth
 
 app = FastAPI()
 
-app.include_router(auth.router, prefix="/auth")
+app.include_router(user_auth.router, prefix="/user-auth")
 app.include_router(music.router, prefix="/music")
+app.include_router(client_auth.router, prefix="/client-auth")
