f1fc2a340e239075a34fc5a1262b5254db3c37c2
mvjkel1 <michal.semen99@gmail.com>
2024-07-21 21:22:37 +0200

    Dotenv improvements

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 
-from . import endpoints
+from endpoints import client_auth, music, user_auth
 
 app = FastAPI()
 
-app.include_router(endpoints.user_auth.router, prefix="/user-auth")
-app.include_router(endpoints.user_auth.router, prefix="/music")
-app.include_router(endpoints.user_auth.router, prefix="/client-auth")
+app.include_router(user_auth.router, prefix="/user-auth")
+app.include_router(music.router, prefix="/music")
+app.include_router(client_auth.router, prefix="/client-auth")
