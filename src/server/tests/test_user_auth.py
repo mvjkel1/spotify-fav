c2e1cad924af4353d5103b7e43d86e3ab05d6ccd
mvjkel1 <michal.semen99@gmail.com>
2024-09-24 20:53:56 +0200

    test_user_auth follow-up

--- src/server/tests/test_user_auth.py
+++ src/server/tests/test_user_auth.py
@@ -3,17 +3,11 @@ from unittest.mock import patch
 import httpx
 import pytest
 import status
-from fastapi import HTTPException, Request
+from app.endpoints.user_auth import get_current_user_id
+from fastapi import HTTPException
 
-from app.endpoints.user_auth import (callback, get_current_user,
-                                     get_current_user_id, login)
-
-from .conftest import db_session, test_client
-
-HEADERS = {
-    "Authorization": "Bearer access_token",
-    "Content-Type": "application/json",
-}
+from .utils.utils import (SPOTIFY_HEADERS_EXAMPLE, USER_DATA_EXAMPLE,
+                          USER_DATA_EXAMPLE_MALFORMED)
 
 USER_DATA = {
     "id": "user123",
@@ -21,169 +15,90 @@ USER_DATA = {
     "email": "testuser@example.com",
 }
 
-MOCK_DOTENV_VALUES = {
-    "CLIENT_ID": "mock_client_id",
-    "SPOTIFY_API_SCOPES": "mock_scope",
-    "REDIRECT_URI": "http://mock_redirect.com",
-    "SPOTIFY_AUTH_URL": "http://mock_auth_url.com",
-}
-
 
 @pytest.fixture
-def mock_config():
-    with patch("app.endpoints.user_auth.config", MOCK_DOTENV_VALUES) as mock:
+def mock_get_spotify_headers():
+    with patch(
+        "app.endpoints.user_auth.get_spotify_headers",
+        return_value=SPOTIFY_HEADERS_EXAMPLE,
+    ) as mock:
         yield mock
 
 
-@pytest.mark.asyncio
-@patch(
-    "app.endpoints.user_auth.httpx.AsyncClient.get",
-    return_value=httpx.Response(
-        200,
-        json=USER_DATA,
-    ),
-)
-@patch("app.endpoints.user_auth.get_spotify_headers", return_value=HEADERS)
+@pytest.fixture
+def mock_async_client_get():
+    with patch(
+        "app.endpoints.user_auth.httpx.AsyncClient.get",
+    ) as mock:
+        yield mock
+
+
+@pytest.fixture
+def mock_get_current_user():
+    with patch("app.endpoints.user_auth.get_current_user") as mock:
+        yield mock
+
+
 @pytest.mark.parametrize("expected_output", [USER_DATA])
-async def test_get_current_user_success(
-    mock_spotify_headers, mock_async_client, expected_output
-):
-    result = await get_current_user()
-    assert result == expected_output
-    mock_async_client.assert_called_once_with(
-        "https://api.spotify.com/v1/me",
-        headers=HEADERS,
-    )
-
-
-@pytest.mark.asyncio
-@patch("app.endpoints.user_auth.get_spotify_headers", return_value=HEADERS)
-@patch(
-    "app.endpoints.user_auth.httpx.AsyncClient.get",
-    side_effect=HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized"
-    ),
-)
-@pytest.mark.parametrize(
-    "expected_status_code, expected_message",
-    [(status.HTTP_401_UNAUTHORIZED, "Unauthorized")],
-)
-async def test_get_current_user_failure(
-    mock_async_client,
-    mock_spotify_headers,
-    expected_status_code,
-    expected_message,
-):
-    with pytest.raises(HTTPException) as exc:
-        await get_current_user()
-
-    assert exc.value.status_code == expected_status_code
-    assert expected_message in exc.value.detail
-
-
-@pytest.mark.asyncio
-@patch("app.endpoints.user_auth.get_current_user", return_value=USER_DATA)
-async def test_get_current_user_id_success(mock_get_current_user, db_session):
-    user_id = await get_current_user_id(db_session)
-    assert user_id == "user123"
-
-
-@pytest.mark.asyncio
-@patch(
-    "app.endpoints.user_auth.get_current_user",
-    side_effect=HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized"
-    ),
-)
-@pytest.mark.parametrize(
-    "expected_status_code, expected_message",
-    [(status.HTTP_401_UNAUTHORIZED, "Unauthorized")],
-)
-async def test_get_current_user_id_failure(
-    mock_get_current_user,
-    db_session,
-    expected_status_code,
-    expected_message,
-):
-    with pytest.raises(HTTPException) as exc:
-        await get_current_user_id(db_session)
-    assert exc.value.status_code == expected_status_code
-    assert expected_message in exc.value.detail
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize(
-    "expected_output",
-    [
-        {
-            "login_url": "http://mock_auth_url.com?response_type=code&client_id=mock_client_id&scope=mock_scope&redirect_uri=http%3A%2F%2Fmock_redirect.com&state=randomstring123"
-        }
-    ],
-)
-@patch("app.endpoints.user_auth.generate_random_string", return_value="randomstring123")
-async def test_login(
-    mock_generate_random_string,
-    mock_config,
+def test_get_current_user_success(
+    mock_async_client_get,
+    mock_get_spotify_headers,
+    test_client,
     expected_output,
 ):
-    response = await login()
-    assert response == expected_output
-
-
-@pytest.mark.asyncio
-async def test_callback_missing_code():
-    request = Request(
-        {
-            "type": "http",
-            "method": "GET",
-            "headers": {},
-            "query_string": "",
-            "url": "url",
-        }
+    mock_async_client_get.return_value = httpx.Response(
+        status_code=200, json=USER_DATA_EXAMPLE
     )
-    with pytest.raises(HTTPException) as exc:
-        await callback(request)
-    assert exc.value.status_code == status.HTTP_400_BAD_REQUEST
-    assert exc.value.detail == "Authorization code not found in request"
+    response = test_client.get("/user-auth/me")
+    assert response.json() == expected_output
+    mock_async_client_get.assert_called_once_with(
+        "https://api.spotify.com/v1/me",
+        headers=SPOTIFY_HEADERS_EXAMPLE,
+    )
 
 
-@pytest.mark.asyncio
-@patch("app.endpoints.user_auth.save_token")
-@patch(
-    "app.endpoints.user_auth.httpx.AsyncClient.post",
-    return_value=httpx.Response(
-        200, json={"access_token": "123", "refresh_token": "321", "expires_in": 0}
-    ),
+@pytest.mark.parametrize(
+    "expected_response",
+    [
+        {
+            "detail": (
+                "Failed to fetch user data: {\n"
+                '  "error": {\n'
+                '    "status": 401,\n'
+                '    "message": "Invalid access token"\n'
+                "  }\n}"
+            )
+        },
+    ],
 )
-async def test_callback_success(mock_mock_async_client, mock_save_token, db_session):
-    query_string = "code=code123"
-    request = Request(
-        {
-            "type": "http",
-            "method": "GET",
-            "headers": {},
-            "query_string": query_string,
-            "url": "url",
-        }
+def test_get_current_user_unauthorized(
+    mock_get_spotify_headers, test_client, expected_response
+):
+    response = test_client.get("/user-auth/me")
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == expected_response
+
+
+def test_get_current_user_failure(
+    mock_async_client_get, mock_get_spotify_headers, test_client
+):
+    mock_async_client_get.side_effect = HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST, detail="ERROR: Bad request"
     )
-    response = await callback(request, db_session)
-    assert response.status_code == status.HTTP_307_TEMPORARY_REDIRECT
-    mock_save_token.assert_called_once_with("123", "321", 0, db_session)
+    response = test_client.get("/user-auth/me")
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json()["detail"] == "ERROR: Bad request"
 
 
 @pytest.mark.asyncio
-async def test_callback_failure(db_session):
-    query_string = "code=code123"
-    request = Request(
-        {
-            "type": "http",
-            "method": "GET",
-            "headers": {},
-            "query_string": query_string,
-            "url": "url",
-        }
-    )
-    with pytest.raises(HTTPException) as exc:
-        await callback(request)
-    assert exc.value.detail == "Failed to retrieve tokens from Spotify API"
-    assert exc.value.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+async def test_get_current_user_id_success(mock_get_current_user, db_session):
+    mock_get_current_user.return_value = USER_DATA_EXAMPLE
+    response = await get_current_user_id(db_session)
+    assert response == "user123"
+
+
+@pytest.mark.asyncio
+async def test_get_current_user_id_failure(mock_get_current_user, db_session):
+    mock_get_current_user.return_value = USER_DATA_EXAMPLE_MALFORMED
+    response = await get_current_user_id(db_session)
+    assert response == ""
--- src/server/tests/utils/utils.py
+++ src/server/tests/utils/utils.py
@@ -1,2 +1,25 @@
+SPOTIFY_HEADERS_EXAMPLE = {
+    "Authorization": "Bearer access_token",
+    "Content-Type": "application/json",
+}
+
+
+USER_DATA_EXAMPLE = {
+    "id": "user123",
+    "display_name": "Test User",
+    "email": "testuser@example.com",
+}
+
+USER_DATA_EXAMPLE_MALFORMED = {
+    "XiXdX": "user123",
+    "display_name": "Test User",
+    "email": "testuser@example.com",
+}
+
+
 def get_model_attributes(instance):
-    return {key: value for key, value in instance.__dict__.items() if not key.startswith("_")}
+    return {
+        key: value
+        for key, value in instance.__dict__.items()
+        if not key.startswith("_")
+    }
