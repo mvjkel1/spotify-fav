1714453b59b83f9e45676185b6a1d99619a5452a
mvjkel1 <michal.semen99@gmail.com>
2024-10-16 22:01:37 +0200

    Tracks tests follow-up

--- src/server/tests/routers/test_tracks_router.py
+++ src/server/tests/routers/test_tracks_router.py
@@ -6,50 +6,133 @@ from ..fixtures.tracks_fixtures import (
     mock_get_current_track,
     mock_is_user_authorized,
     mock_poll_playback_state,
+    mock_get_playback_state,
+    mock_get_recently_played_tracks,
 )
 from ..fixtures.constants import TRACK_DATA_DICT_EXAMPLE
 
-
-def test_get_current_track_success(test_client, db_session, mock_get_current_track):
-    mock_get_current_track.return_value = TRACK_DATA_DICT_EXAMPLE
-    response = test_client.get("/tracks/current-track")
-    assert response.status_code == 200
-    assert response.json() == TRACK_DATA_DICT_EXAMPLE
-    mock_get_current_track.assert_awaited_with(db_session)
+PATH = "/tracks"
 
 
-def test_get_current_track_failure(test_client, db_session, mock_get_current_track):
-    mock_get_current_track.side_effect = HTTPException(status.HTTP_404_NOT_FOUND)
-    response = test_client.get("/tracks/current-track")
-    assert response.status_code == status.HTTP_404_NOT_FOUND
-    assert response.json() == {"detail": "Not Found"}
-    mock_get_current_track.assert_awaited_with(db_session)
-
-
-def test_poll_success(
+@pytest.mark.parametrize(
+    "mock_return_value, mock_side_effect, expected_status, expected_response",
+    [
+        (TRACK_DATA_DICT_EXAMPLE, None, 200, TRACK_DATA_DICT_EXAMPLE),
+        (
+            None,
+            HTTPException(status.HTTP_404_NOT_FOUND),
+            status.HTTP_404_NOT_FOUND,
+            {"detail": "Not Found"},
+        ),
+    ],
+)
+def test_get_current_track(
     test_client,
     db_session,
     mock_get_current_track,
-    mock_is_user_authorized,
-    mock_poll_playback_state,
+    mock_return_value,
+    mock_side_effect,
+    expected_status,
+    expected_response,
 ):
-    mock_is_user_authorized.return_value = True
-    response = test_client.post("/tracks/poll")
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {"message": "Playback state polling started in the background."}
-    mock_poll_playback_state.assert_awaited_once_with(db_session)
+    mock_get_current_track.return_value = mock_return_value
+    mock_get_current_track.side_effect = mock_side_effect
+    response = test_client.get(f"{PATH}/current-track")
+    assert response.status_code == expected_status
+    assert response.json() == expected_response
+    mock_get_current_track.assert_awaited_with(db_session)
 
 
-def test_poll_failure_unauthorized(
+@pytest.mark.parametrize(
+    "is_authorized, expected_status, expected_response",
+    [
+        (
+            True,
+            status.HTTP_200_OK,
+            {"message": "Playback state polling started in the background."},
+        ),
+        (
+            False,
+            status.HTTP_401_UNAUTHORIZED,
+            {"detail": "Unauthorized - to start the polling you have to login first."},
+        ),
+    ],
+)
+def test_poll(
     test_client,
     db_session,
-    mock_get_current_track,
     mock_is_user_authorized,
     mock_poll_playback_state,
+    is_authorized,
+    expected_status,
+    expected_response,
 ):
-    mock_is_user_authorized.return_value = False
-    response = test_client.post("/tracks/poll")
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
-    assert response.json() == {
-        "detail": "Unauthorized - to start the polling you have to login first."
-    }
+    mock_is_user_authorized.return_value = is_authorized
+    response = test_client.post(f"{PATH}/poll")
+    assert response.status_code == expected_status
+    assert response.json() == expected_response
+    if is_authorized:
+        mock_poll_playback_state.assert_awaited_once_with(db_session)
+
+
+@pytest.mark.parametrize(
+    "mock_return_value, mock_side_effect, expected_status, expected_response",
+    [
+        (
+            {"tracks": "track1", "tracks": "track2"},
+            None,
+            status.HTTP_200_OK,
+            {"tracks": "track1", "tracks": "track2"},
+        ),
+        (
+            None,
+            HTTPException(status.HTTP_404_NOT_FOUND),
+            status.HTTP_404_NOT_FOUND,
+            {"detail": "Not Found"},
+        ),
+    ],
+)
+def test_get_recently_played(
+    test_client,
+    db_session,
+    mock_get_recently_played_tracks,
+    mock_return_value,
+    mock_side_effect,
+    expected_status,
+    expected_response,
+):
+    mock_get_recently_played_tracks.return_value = mock_return_value
+    mock_get_recently_played_tracks.side_effect = mock_side_effect
+    response = test_client.get(f"{PATH}/recently-played")
+    assert response.status_code == expected_status
+    assert response.json() == expected_response
+    mock_get_recently_played_tracks.assert_awaited_with(db_session, 1)
+
+
+@pytest.mark.parametrize(
+    "mock_return_value, mock_side_effect, expected_status, expected_response",
+    [
+        ({"state": "playing"}, None, status.HTTP_200_OK, {"state": "playing"}),
+        (
+            None,
+            HTTPException(status.HTTP_404_NOT_FOUND),
+            status.HTTP_404_NOT_FOUND,
+            {"detail": "Not Found"},
+        ),
+    ],
+)
+def test_get_playback_state(
+    test_client,
+    db_session,
+    mock_get_playback_state,
+    mock_return_value,
+    mock_side_effect,
+    expected_status,
+    expected_response,
+):
+    mock_get_playback_state.return_value = mock_return_value
+    mock_get_playback_state.side_effect = mock_side_effect
+    response = test_client.get(f"{PATH}/playback-state")
+    assert response.status_code == expected_status
+    assert response.json() == expected_response
+    mock_get_playback_state.assert_awaited_with(db_session)
