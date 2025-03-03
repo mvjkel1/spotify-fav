d1bebd7f407e86a67196edc4dff48be96009c069
mvjkel1 <michal.semen99@gmail.com>
2025-03-03 18:03:56 +0100

    Add poll to collect listened track (initial version) 	.Simplified Track model

--- src/models.py
+++ src/models.py
@@ -7,5 +7,3 @@ class Track(Base):
     __tablename__ = "tracks"
     id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False)
-    spotify_id = Column(Integer, nullable=False)
-    listened = Column(Boolean, nullable=False, default=False)
