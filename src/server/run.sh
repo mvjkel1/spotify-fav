4771d8dc8c430791f5ec46f4a8e32f4572c2eaab
mvjkel1 <michal.semen99@gmail.com>
2025-03-03 18:04:06 +0100

    Add formatting possibility to the run.sh script

--- src/server/run.sh
+++ src/server/run.sh
@@ -8,6 +8,7 @@ Usage:
   ./run.sh coverage          - Run tests with coverage
   ./run.sh coverage_report   - Run tests with coverage and generate a report
   ./run.sh coverage_html     - Run tests with coverage and generate an HTML report
+  ./run.sh formatting        - Run black & isort on recently changes files
   ./run.sh help              - Show this help message
 EOF
 }
@@ -43,6 +44,14 @@ function run_coverage() {
     esac
 }
 
+function run_formatting() {
+    for file in $(git status --porcelain | awk '{print $2}' | grep '\.py$'); do
+        trimmed_file_path="${file#src/server/}"
+        isort "$trimmed_file_path"
+        black -l 100 "$trimmed_file_path"
+    done
+}
+
 function main() {
     case $1 in
         server)
@@ -62,6 +71,9 @@ function main() {
             ;;
         coverage_html)
             run_coverage html
+            ;;
+        format)
+            run_formatting
             ;;
         help)
             show_help
