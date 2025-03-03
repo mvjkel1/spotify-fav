6cb43a354be2a2dc600060c35c4d5d4c7aa6a1f4
mvjkel1 <michal.semen99@gmail.com>
2025-03-03 18:03:57 +0100

    test_user_auth continuation + format.sh + requirements.txt update

--- src/server/tests/test_user_auth.py
+++ src/server/tests/test_user_auth.py
@@ -1,10 +1,15 @@
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 import httpx
 import pytest
 import status
-from app.endpoints.user_auth import get_current_user, get_current_user_id
-from fastapi import HTTPException
+from app.endpoints.user_auth import (
+    callback,
+    get_current_user,
+    get_current_user_id,
+    login,
+)
+from fastapi import HTTPException, Request
 
 from .conftest import db_session, test_client
 
@@ -18,6 +23,19 @@ USER_DATA = {
     "display_name": "Test User",
     "email": "testuser@example.com",
 }
+
+MOCK_DOTENV_VALUES = {
+    "CLIENT_ID": "mock_client_id",
+    "SPOTIFY_API_SCOPES": "mock_scope",
+    "REDIRECT_URI": "http://mock_redirect.com",
+    "SPOTIFY_AUTH_URL": "http://mock_auth_url.com",
+}
+
+
+@pytest.fixture
+def mock_config():
+    with patch("app.endpoints.user_auth.config", MOCK_DOTENV_VALUES) as mock:
+        yield mock
 
 
 @pytest.mark.asyncio
@@ -92,3 +110,40 @@ async def test_get_current_user_id_failure(
         await get_current_user_id(db_session)
     assert exc.value.status_code == expected_status_code
     assert expected_message in exc.value.detail
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "expected_output",
+    [
+        {
+            "login_url": "http://mock_auth_url.com?response_type=code&client_id=mock_client_id&scope=mock_scope&redirect_uri=http%3A%2F%2Fmock_redirect.com&state=randomstring123"
+        }
+    ],
+)
+@patch("app.endpoints.user_auth.generate_random_string", return_value="randomstring123")
+async def test_login(
+    mock_generate_random_string,
+    test_client,
+    mock_config,
+    expected_output,
+):
+    response = await login()
+    assert response == expected_output
+
+
+@pytest.mark.asyncio
+async def test_callback_missing_code(test_client):
+    request = Request(
+        {
+            "type": "http",
+            "method": "GET",
+            "headers": {},
+            "query_string": b"",
+            "url": "url",
+        }
+    )
+    with pytest.raises(HTTPException) as exc:
+        response = await callback(request)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.json() == {"detail": "Authorization code not found in request"}
