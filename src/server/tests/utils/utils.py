bef8deb9b89ae476885452deaae0f2d9fa915a28
mvjkel1 <karasmichal3005@gmail.com>
2025-03-24 22:39:39 +0100

    User auth service test cases adjustments (to be followed up)

--- src/server/tests/utils/utils.py
+++ src/server/tests/utils/utils.py
@@ -1,9 +1,8 @@
 from sqlalchemy import insert
 from sqlalchemy.ext.asyncio.session import AsyncSession
 
-from app.db.models import Track, User, user_track_association_table
+from app.db.models import User, user_track_association_table
 from tests.fixtures.constants import (
-    EXAMPLE_TRACK_ID,
     SPOTIFY_USER_ID_EXAMPLE,
     TRACK_EXAMPLE_DB,
     USER_ID_EXAMPLE,
