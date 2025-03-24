40372eebcf6740729eb743be93265c6aeddc7aa8
mvjkel1 <karasmichal3005@gmail.com>
2025-03-24 22:39:40 +0100

    Test user auth imports adjustment

--- src/server/tests/services/test_user_auth_service.py
+++ src/server/tests/services/test_user_auth_service.py
@@ -1,15 +1,10 @@
 import pytest
+from app.services.user_auth_service import create_refresh_token, get_current_user
 from fastapi import HTTPException, status
 
-from app.services.user_auth_service import create_refresh_token, get_current_user
+from ..fixtures.constants import USER_EXAMPLE_DB
+from ..fixtures.services.user_auth_service_fixtures import mock_config_env
 from ..utils.utils import get_model_attributes
-
-from ..fixtures.constants import (
-    USER_EXAMPLE_DB,
-)
-from ..fixtures.services.user_auth_service_fixtures import (
-    mock_config_env,
-)
 
 
 @pytest.mark.asyncio
