78a18e053a0e9126e3c5818cfac70691ef2d6505
mvjkel1 <karasmichal3005@gmail.com>
2025-03-24 22:39:05 +0100

    Update run.sh run_server function

--- src/server/run.sh
+++ src/server/run.sh
@@ -11,7 +11,7 @@ EOF
 
 function run_server() {
     echo "Starting API server..."
-    uvicorn app.main:app --reload
+    uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
 }
 
 function run_tests() {
