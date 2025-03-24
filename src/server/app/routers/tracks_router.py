c9b0545c9b72abdb4179e6651139ca92c1ec07ec
mvjkel1 <karasmichal3005@gmail.com>
2025-03-24 22:39:04 +0100

    User auth service refactor and tracks polling follow-up

--- src/server/app/routers/tracks_router.py
+++ src/server/app/routers/tracks_router.py
@@ -5,28 +5,72 @@ from app.services.tracks_service import (
     get_recently_played_track,
     poll_playback_state,
 )
-from fastapi import APIRouter, BackgroundTasks, Depends
+from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException
 from sqlalchemy.orm import Session
+
+from app.services.user_auth_service import is_user_authorized
 
 router = APIRouter(tags=["tracks"], prefix="/tracks")
 
 
 @router.get("/current-track")
 async def current_track(db_session: Session = Depends(get_db)) -> dict:
+    """
+    Retrieve the current track being played.
+
+    Args:
+        db_session (Session): The database session to use.
+
+    Returns:
+        dict: A dictionary containing details of the current track.
+    """
     return await get_current_track(db_session)
 
 
 @router.post("/poll")
 async def poll(background_tasks: BackgroundTasks, db_session: Session = Depends(get_db)) -> dict:
-    background_tasks.add_task(poll_playback_state, db_session)
-    return {"message": "Playback state polling started in the background."}
+    """
+    Start polling the playback state in the background.
+
+    Args:
+        background_tasks (BackgroundTasks): The background task manager.
+        db_session (Session): The database session to use.
+
+    Returns:
+        dict: A message indicating that polling has started.
+    """
+    # Workaround to don't add background tasks if there was an exception within poll_playback_state func call,
+    # it should be improved in the future
+    if is_user_authorized(db_session):
+        background_tasks.add_task(poll_playback_state, db_session)
+        return {"message": "Playback state polling started in the background."}
+    raise HTTPException(404, "Unauthorized - to start the polling you have to login first.")
 
 
 @router.get("/recently-played")
-async def get_recently_played(db_session: Session = Depends(get_db), limit: int = 1) -> dict:
+async def get_recently_played(limit: int = 1, db_session: Session = Depends(get_db)) -> dict:
+    """
+    Retrieve recently played tracks.
+
+    Args:
+        db_session (Session): The database session to use.
+        limit (int): The number of recently played tracks to retrieve. Default is 1.
+
+    Returns:
+        dict: A dictionary containing details of the recently played tracks.
+    """
     return await get_recently_played_track(db_session, limit)
 
 
 @router.get("/playback-state")
 async def playback_state(db_session: Session = Depends(get_db)) -> dict:
+    """
+    Retrieve the current playback state.
+
+    Args:
+        db_session (Session): The database session to use.
+
+    Returns:
+        dict: A dictionary containing details of the current playback state.
+    """
     return await get_playback_state(db_session)
