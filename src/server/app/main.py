f349b10ec251dd0abb99eb3c3821548feb49e985
mvjkel1 <karasmichal3005@gmail.com>
2025-05-05 14:20:50 +0200

    Add prometheus to capture the requests duration

--- src/server/app/main.py
+++ src/server/app/main.py
@@ -1,12 +1,19 @@
 from contextlib import asynccontextmanager
 
+from fastapi import FastAPI, Request
+from prometheus_client import Histogram, make_wsgi_app
+from starlette.middleware.wsgi import WSGIMiddleware
+
 import app.routers.playlists_router as playlists_router
 import app.routers.spotify_auth_router as spotify_auth_router
 import app.routers.tracks_router as tracks_router
 import app.routers.user_auth_router as user_auth_router
 from app.db.database import async_get_db
 from app.services.tracks_service import update_polling_status
-from fastapi import FastAPI
+
+REQUEST_DURATION = Histogram(
+    "http_request_duration_seconds", "Duration of HTTP requests in seconds", ["method", "endpoint"]
+)
 
 
 @asynccontextmanager
@@ -20,7 +27,19 @@ async def app_lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=app_lifespan)
 
+
+@app.middleware("http")
+async def track_request_duration(request: Request, call_next):
+    method = request.method
+    endpoint = request.url.path
+
+    with REQUEST_DURATION.labels(method=method, endpoint=endpoint).time():
+        response = await call_next(request)
+    return response
+
+
 app.include_router(user_auth_router.router)
 app.include_router(tracks_router.router)
 app.include_router(playlists_router.router)
 app.include_router(spotify_auth_router.router)
+app.mount("/metrics", WSGIMiddleware(make_wsgi_app()))
