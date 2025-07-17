84b8529515815fa99ab19d20d9b87c89d805fa9c
mvjkel1 <karasmichal3005@gmail.com>
2025-07-17 11:19:45 +0200

    User auth service tests update

--- src/server/tests/services/test_user_auth_service.py
+++ src/server/tests/services/test_user_auth_service.py
@@ -1,27 +1,30 @@
 import pytest
-from app.services.user_auth_service import create_refresh_token, get_current_user
 from fastapi import HTTPException, status
 
+from app.services.user_auth_service import create_refresh_token, get_current_user
+
+from ..conftest import db_session
 from ..fixtures.constants import USER_EXAMPLE_DB
 from ..fixtures.services.user_auth_service_fixtures import mock_config_env
 from ..utils.utils import get_model_attributes
 
-# @pytest.mark.asyncio
-# async def test_get_current_user_success(db_session):
-#     test_user = USER_EXAMPLE_DB
-#     jwt_token = create_refresh_token(data={"sub": test_user.email})
-#     db_session.add(test_user)
-#     await db_session.commit()
-#     result = await get_current_user(jwt_token, db_session)
-#     assert get_model_attributes(result) == {
-#         "id": 1,
-#         "spotify_uid": "1",
-#         "hashed_password": "P!w!D",
-#         "is_active": True,
-#         "email": "user@example.com",
-#         "is_polling": False,
-#         "last_login": None,
-#     }
+
+@pytest.mark.asyncio
+async def test_get_current_user_success(db_session):
+    test_user = USER_EXAMPLE_DB
+    jwt_token = create_refresh_token(data={"sub": test_user.email})
+    db_session.add(test_user)
+    await db_session.commit()
+    result = await get_current_user(jwt_token, db_session)
+    assert get_model_attributes(result) == {
+        "id": 1,
+        "spotify_uid": "1",
+        "hashed_password": "P!w!D",
+        "is_active": True,
+        "email": "user@example.com",
+        "is_polling": False,
+        "last_login": None,
+    }
 
 
 @pytest.mark.asyncio
